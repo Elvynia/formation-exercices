a4a38d60c6fa7673e697885af6a1f3b9282d864a
elvynia <elvynia@gmail.com>
2017-05-04 09:50:19 +0200

    Ajout du document avec les énoncés ainsi que les corrections des exercices 1 à 12

---
+++ src/fr/lyon/formation/exo11/utensil/SquarePlate.java
@@ -0,0 +1,47 @@
+package fr.lyon.formation.exo11.utensil;
+
+/**
+ * Classe représentant une assiette carré. Cette classe implémente Plate car
+ * elle possède une méthode permettant de calculer sa surface. Celle classe est
+ * aussi une sous classe de Utensil car une assiette est un ustensile.
+ *
+ * @author hb-asus
+ *
+ */
+public class SquarePlate extends Utensil implements Plate {
+	public final double score;
+
+	/**
+	 * Constructeur.
+	 *
+	 * @param year
+	 *            l'année de création de l'assiette utilisée dans la classe
+	 *            Utensil.
+	 * @param score
+	 *            la cote de l'assiette (taille d'un côté).
+	 */
+	public SquarePlate(int year, double score) {
+		// Appel du constructeur de la classe Utensil.
+		super(year);
+		this.score = score;
+	}
+
+	/**
+	 * {@inheritDoc} Calcul de la surface à l'aide de la cote (taille d'un côté
+	 * de l'assiette).
+	 */
+	@Override
+	public double calculateArea() {
+		return Math.pow(this.score, 2);
+	}
+
+	/**
+	 * {@inheritDoc} La valeur d'une assiette carré est 5 fois la valeur d'un
+	 * ustensile commun.
+	 */
+	@Override
+	public int calculateValue(int currentYear) {
+		return super.calculateValue(currentYear) * 5;
+	}
+
+}
